49340edcad124ba063ec2e8a427e1d28ca8ad109
carmellodusci@yahoo.com <>
2018-10-22 15:32:53 -0400

    First Commit

---
+++ src/githubtest/GitHubTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author jcd11
+ */
+public class GitHubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+      Dusci();
+    }
+    
+    public static void Dusci()
+    {
+        System.out.println("Jeno Dusci");
+    }
+    
+}
